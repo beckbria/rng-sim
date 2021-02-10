28b5a518635f3ac3987fd49164440ed18d60a343
Brian Beck <brian.beck@outlook.com>
2021-02-10 13:05:58 -0800

    Add ability to provide initial deck for debugging

--- blackjack.js
+++ blackjack.js
@@ -1,16 +1,19 @@
 import { CardHandlerUnit } from "./chu.js";
 import { shuffleArray } from './helpers.js';
 
+const sortedFullDeck = [
+    "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS",
+    "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH",
+    "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD",
+    "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC",
+];
+sortedFullDeck.sort();
+
 class BlackjackGame {
     constructor(useLocalStorage, controlProgram = "", program1 = "", program2 = "", initialDeck = null) {
         var fullDeck = initialDeck;
         if (fullDeck == null) {
-            fullDeck = [
-                "AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "TS", "JS", "QS", "KS",
-                "AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "TH", "JH", "QH", "KH",
-                "AD", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "TD", "JD", "QD", "KD",
-                "AC", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "TC", "JC", "QC", "KC",
-            ];
+            fullDeck = [...sortedFullDeck];
             shuffleArray(fullDeck);
         }
 
@@ -41,7 +44,6 @@ class BlackjackGame {
         this.ch1.right = appendCh2;
 
         // Hook up the deal action of each unit
-        this.controlCh.deal = function (card) {};
         this.ch1.deal = function (card) {
             that.dealerCards.push(card);
             that.updateDealtCards();
@@ -109,6 +111,25 @@ class BlackjackGame {
             }
         }
         return success;
+    }
+
+    static validDeck(deck) {
+        if (!Array.isArray(deck)) {
+            return false;
+        }
+        const copy = [...deck];
+        copy.sort();
+        return copy.length === sortedFullDeck.length && copy.every((card, index) => card === sortedFullDeck[index]);
+    }
+
+    setInitialDeck(deck) {
+        if (BlackjackGame.validDeck(deck)) {
+            this.controlCh.initialInputQueue = deck;
+            this.controlCh.inputQueue = deck;
+            this.controlCh.updateRegisters();
+            return true;
+        }
+        return false;
     }
 
     resetState() {
@@ -302,6 +323,7 @@ class BlackjackGameWithUi extends BlackjackGame {
 
     resetState() {
         if (!this.editMode) {
+            document.getElementById('deck_button').disabled = false;
             this.disableStepButtons(false);
         }
         document.getElementById('validation_state_wrapper').style.visibility = 'collapse';
@@ -315,6 +337,8 @@ class BlackjackGameWithUi extends BlackjackGame {
     }
 
     nextLine() {
+        // Once we've started the program we can no longer change the initial deck
+        document.getElementById('deck_button').disabled = true;
         const success = super.nextLine();
         if (!success) {
             // Disable the "next" button since we can't advance.  Leave the "reset" button enabled
@@ -329,7 +353,7 @@ class BlackjackGameWithUi extends BlackjackGame {
     }
 
     disableStepButtons(disabled) {
-        const stepButtons = ['next_button', 'run_button'].map(function(id) { return document.getElementById(id); });
+        const stepButtons = ['next_button', 'run_button', 'deck_button'].map(function(id) { return document.getElementById(id); });
         for (const b of stepButtons) {
             b.disabled = disabled;
         }
